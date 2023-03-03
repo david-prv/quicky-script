945ff6410cfbb9980826b32a2039356d3f51fab2
David <66866223+david-prv@users.noreply.github.com>
2023-03-03 11:50:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
+import sys
+import time
 from QuickyScript import lexer
+from QuickyScript import parser
 
-with open('sample1.qcnf') as f: s = f.read()
+"""
+Next:
+- Allow numbers for config name
+- Allow numbers for callback
+- Automatic Code Generation
 
-print(lexer.tokenize(s))
+Future:
+- Allow more all relevant symbols for routes (@, *, {, })
+- Add parameterized callbacks
+"""
+
+if (len(sys.argv) <= 1):
+    exit("USAGE: python3 app.py config [config [config [config [...]]]]")
+
+st = time.time()
+
+for config in sys.argv[1:]:
+    if config.split(".")[1] != "qcnf":
+        exit("ERROR: Provided file is not a Quicky-Config")
+
+    with open(config) as f: s = f.read()
+    cnf = parser.parse(lexer.tokenize(s))
+    
+    print(cnf.dump())
+
+fin = time.time()
+
+delta = fin - st
+print(f"\nTook {delta} seconds")
